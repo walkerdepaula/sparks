e0d4c6506af720c56a36c71da5ae3cd6465f1b0d
walkerdepaula <walkerdepaula.work@gmail.com>
2017-12-11 19:50:53 -0200

    adicionando e configurando vuex

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import state from './state'
+
+// modules import
+// import insertData from '../components/insert-data/store'
+
+// Components
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  mutations,
+  state,
+  // modules: {
+  //   insertData
+  // }
+})
